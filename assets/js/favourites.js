54189f9791c56d6a68e140fae7355a2e225bcb98
sarasg89 <118869836+sarasg89@users.noreply.github.com>
2023-02-03 20:51:10 -0500

    added event listener to the button to delete a recipe

--- assets/js/favourites.js
+++ assets/js/favourites.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
 
     function displayRecipe(data) {
         // The contents of this div are dynamically created with JS
-        console.log(data);
         var html = 
             '<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">' +
             '<div class="flex flex-col items-center pb-10">' +
@@ -15,29 +14,41 @@ $(document).ready(function () {
             // '<p id="recipe-instructions-' + data.idMeal + '" class="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>' +
             '<div class="flex mt-4 space-x-3 md:mt-6">' +
             '<a id="recipe-link-' + data.idMeal + '" href="#" target="_blank" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Link to the recipe<svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></a>' +
-            '<button id="delete-recipe-' + data.idMeal + '" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Delete this recipe</button>' + '</div></div></div>'
+            '<button data-meal=' + data.idMeal +  ' id="delete-recipe-' + data.idMeal + '" class="delete-btn inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Delete this recipe</button>' + '</div></div></div>'
 
         // The html variable is added to the div created in HTML
         $("#fav-recipes-container").append(html);
 
         $("#recipe-heading-" + data.idMeal).text(data.strMeal);
-        $("#recipe-instructions-" + data.idMeal).text(data.strInstructions);
+        // $("#recipe-instructions-" + data.idMeal).text(data.strInstructions);
         $("#recipe-link-" + data.idMeal).attr("href", data.strSource);
-        $("#cuisine-" + data.idMeal).text(data.strArea);
-        $("#category-" + data.idMeal).text(data.strCategory);
+        
 
-        for (var i = 1; i < 22; i++) {
-            var strIng = "strIngredient" + i;
-            var strMeas = "strMeasure" + i;
-            if (data[strIng] === "") {
-                return;
-            } else if (data[strIng] === null) {
-                return;
-            } else {
-                $("#recipe-ingredients-list").append("<li>" + data[strMeas] + " " + data[strIng] + "<li");
-            }
-        };
+        // for (var i = 1; i < 22; i++) {
+        //     var strIng = "strIngredient" + i;
+        //     var strMeas = "strMeasure" + i;
+        //     if (data[strIng] === "") {
+        //         return;
+        //     } else if (data[strIng] === null) {
+        //         return;
+        //     } else {
+        //         $("#recipe-ingredients-list").append("<li>" + data[strMeas] + " " + data[strIng] + "<li");
+        //     }
+        // };
     }
+
+    // Event listener for the delete recipe button
+    $(document).on("click", ".delete-btn", function(event){
+        var recipeDeleted = $(this).attr("data-meal");
+        var recipeStored = JSON.parse(localStorage.getItem("recipe"));
+
+        // This for loop checks that if the recipe ID of the button clicked matches the recipe ID of local storage, that recipe is deleted from local storage
+        for (var i = 0; i < recipeStored.length; i++) {
+            if (recipeDeleted === recipeStored[i]) {
+                localStorage.removeItem("recipe", JSON.stringify(recipeDeleted));
+            }
+        }
+    })
 
 
     function init() {
@@ -53,7 +64,7 @@ $(document).ready(function () {
         }
 
 
-        // For as many id as are saved to local storage fetch the API based on the specific ID saved and display it to the screen
+        // For as many IDs as are saved to local storage fetch the API based on the specific ID saved and display it to the screen
         for (var i = 0; i < recipeId.length; i++) {
             fetch(mealDBAPIById + recipeId[i])
                 .then(function (response) {

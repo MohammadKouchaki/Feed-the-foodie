53e2f969991cd92a1065c76c68bb68ae5b4abaa8
sarasg89 <118869836+sarasg89@users.noreply.github.com>
2023-02-04 16:02:15 -0500

    added a form for types of cuisine and an event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,50 @@
 $(document).ready(function () {
-    var mealDBApi = "https://www.themealdb.com/api/json/v2/9973533/filter.php?i=";
+    var mealDBApi = "https://www.themealdb.com/api/json/v2/9973533/filter.php?";
     var meatsEl = $("#meats");
     var vegetablesEl = $("#vegetables");
     var fruitsEl = $("#fruits");
     var beansEl = $("#beans")
-    var submitBtnEl = $("button");
+    var submitBtnIEl = $("#ingredients-btn");
+    var submitBtnCEl = $("#cuisine-btn")
+    var cuisineEl = $("#cuisine")
 
-    // Event listener for the submit button
-    submitBtnEl.on("click", function (event) {
+    // Event listener for the submit button (ingredients)
+    submitBtnIEl.on("click", function (event) {
         event.preventDefault();
-        var newUrl = ""
+        var newUrlIngredients = ""
 
         if (meatsEl.val() !== null && vegetablesEl.val() !== null && fruitsEl.val() !== null && beansEl.val() !== null) {
-            newUrl = `${mealDBApi}${meatsEl.val()},${vegetablesEl.val()},${fruitsEl.val()},${beansEl.val()}`;
+            newUrlIngredients = `i=${mealDBApi}${meatsEl.val()},${vegetablesEl.val()},${fruitsEl.val()},${beansEl.val()}`;
         } else if (meatsEl.val() !== null && vegetablesEl.val() !== null && fruitsEl.val() !== null && beansEl.val() === null) {
-            newUrl = `${mealDBApi}${meatsEl.val()},${vegetablesEl.val()},${fruitsEl.val()}`;
+            newUrlIngredients = `i=${mealDBApi}${meatsEl.val()},${vegetablesEl.val()},${fruitsEl.val()}`;
         } else if (meatsEl.val() !== null && vegetablesEl.val() !== null && fruitsEl.val() === null && beansEl.val() === null) {
-            newUrl = `${mealDBApi}${meatsEl.val()},${vegetablesEl.val()}`;
+            newUrlIngredients = `i=${mealDBApi}${meatsEl.val()},${vegetablesEl.val()}`;
         } else if ((meatsEl.val() !== null && vegetablesEl.val() === null && fruitsEl.val() === null && beansEl.val() === null)) {
-            newUrl = `${mealDBApi}${meatsEl.val()}`;
+            newUrlIngredients = `i=${mealDBApi}${meatsEl.val()}`;
         } else {
-            newUrl = "https://www.themealdb.com/api/json/v2/9973533/random.php"
+            newUrlIngredients = "https://www.themealdb.com/api/json/v2/9973533/random.php"
         }
-        
-        console.log(newUrl)
 
-        fetch(newUrl)
+        console.log(newUrlIngredients)
+
+        fetch(newUrlIngredients)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                console.log(data)
+            })
+    })
+
+    // Event listener for the submit button (cuisine)
+    submitBtnCEl.on("click", function (event) {
+        event.preventDefault();
+        var newUrlCuisine = mealDBApi + "a=" + cuisineEl.val();
+
+        console.log(newUrlCuisine)
+
+
+        fetch(newUrlCuisine)
             .then(function (response) {
                 return response.json()
             })

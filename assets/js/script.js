73043530e7b9a13641ac58520b54a887b717abaa
sarasg89 <118869836+sarasg89@users.noreply.github.com>
2023-02-05 14:21:46 -0500

    added a function to display the instructions and ingredients for each recipe

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,27 +13,77 @@ $(document).ready(function () {
     var submitBtnMealEl = $("#meal-btn");
     var mealEl = $("#meal");
 
-    
+
     // ***************
     // DISPLAY RECIPES
     // ***************
 
     function displayRecipe(data) {
-       
+        $("#recipe-display").removeAttr("hidden");
+
         // Recipe 1
         $("#recipe-heading1").text(data[0].title)
         $("#recipe-img1").attr("src", data[0].image)
+        $("#recipe-ingredients-list1").text()
+
 
         // Recipe 2
         $("#recipe-heading2").text(data[1].title)
         $("#recipe-img2").attr("src", data[1].image)
+        $("#recipe-ingredients-list2").text()
+
 
         // Recipe 3
         $("#recipe-heading3").text(data[2].title)
         $("#recipe-img3").attr("src", data[2].image)
+        $("#recipe-ingredients-list3").text()
+
     }
 
-    
+    function getInfo(data) {
+        var spoonacularUrlId1 = "https://api.spoonacular.com/recipes/" + data[0].id + "/information?&apiKey=34486790fc234b2daa001d801bc76511"
+        var spoonacularUrlId2 = "https://api.spoonacular.com/recipes/" + data[1].id + "/information?&apiKey=34486790fc234b2daa001d801bc76511"
+        var spoonacularUrlId3 = "https://api.spoonacular.com/recipes/" + data[2].id + "/information?&apiKey=34486790fc234b2daa001d801bc76511"
+
+        // Recipe 1
+        fetch(spoonacularUrlId1)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                $("#recipe-instructions1").append(data.instructions);
+                for (var i = 0; i < data.extendedIngredients.length; i++) {
+                    $("#recipe-ingredients-list1").append(`<li>${data.extendedIngredients[i].measures.us.amount} ${data.extendedIngredients[i].measures.us.unitShort} ${data.extendedIngredients[i].name}</li>`);
+                };
+            })
+
+        // Recipe 2
+        fetch(spoonacularUrlId2)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                $("#recipe-instructions2").append(data.instructions);
+                for (var i = 0; i < data.extendedIngredients.length; i++) {
+                    $("#recipe-ingredients-list2").append(`<li>${data.extendedIngredients[i].measures.us.amount} ${data.extendedIngredients[i].measures.us.unitShort} ${data.extendedIngredients[i].name}</li>`);
+                };
+            })
+
+        // Recipe 2
+        fetch(spoonacularUrlId3)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                $("#recipe-instructions3").append(data.instructions);
+                for (var i = 0; i < data.extendedIngredients.length; i++) {
+                    $("#recipe-ingredients-list3").append(`<li>${data.extendedIngredients[i].measures.us.amount} ${data.extendedIngredients[i].measures.us.unitShort} ${data.extendedIngredients[i].name}</li>`);
+                };
+            })
+    }
+
+
+
     // ********************
     // SEARCH BY INGREDIENT
     // ********************
@@ -121,10 +171,11 @@ $(document).ready(function () {
 
         fetch(newUrlIngredients)
             .then(function (response) {
-                return response.json()
+                return response.json();
             })
             .then(function (data) {
-                displayRecipe(data)
+                displayRecipe(data);
+                getInfo(data);
             })
     })
 
@@ -140,11 +191,11 @@ $(document).ready(function () {
 
         fetch(newUrlCuisine)
             .then(function (response) {
-                return response.json()
+                return response.json();
             })
             .then(function (data) {
                 displayRecipe(data.results);
-                console.log(data.results);
+                getInfo(data.results);
             })
     })
 
@@ -159,11 +210,11 @@ $(document).ready(function () {
 
         fetch(newUrlMeal)
             .then(function (response) {
-                return response.json()
+                return response.json();
             })
             .then(function (data) {
-                displayRecipe(data.results)
-                console.log(data.results)
+                displayRecipe(data.results);
+                getInfo(data.results);
             })
     })
 

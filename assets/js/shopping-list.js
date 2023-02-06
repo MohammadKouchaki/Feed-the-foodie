c03176ca09c8c76c69e0e126881254fa7cdfd571
sarasg89 <118869836+sarasg89@users.noreply.github.com>
2023-02-05 19:10:09 -0500

    updated styling as per Hanna's design

--- assets/js/shopping-list.js
+++ assets/js/shopping-list.js
@@ -1,9 +1,22 @@
-    // ******************
-    // SHOPPING-LIST.HTML
-    // ******************
+// ******************
+// SHOPPING-LIST.HTML
+// ******************
 
 
 $(document).ready(function () {
+    var menuBtn = document.getElementById('menu-button');
+    var menu = document.getElementById('menu');
+    var barsIcon = document.getElementById('bars');
+    var xmarkIcon = document.getElementById('xmark');
+
+    menuBtn.addEventListener('click', () => {
+        menu.classList.toggle('hidden');
+        barsIcon.classList.toggle('hidden');
+        xmarkIcon.classList.toggle('hidden');
+    })
+
+
+
     var clearBtnEl = $("#clear-btn");
     var listEl = $("#ingredients-list");
     var printEl = $("#print-btn");

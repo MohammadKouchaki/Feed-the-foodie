2b13f092172ef2ca23e2a8424236c0329dddf2aa
hannazo <106128732+hannazo@users.noreply.github.com>
2023-02-06 23:10:07 -0500

    completed design

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var menuBtn = document.getElementById('menu-button');
+var menu = document.getElementById('menu');
+var barsIcon = document.getElementById('bars');
+var xmarkIcon = document.getElementById('xmark');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+    barsIcon.classList.toggle('hidden');
+    xmarkIcon.classList.toggle('hidden');
+})
+
